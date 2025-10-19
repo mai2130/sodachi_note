bffeadd70c4d9e29edfd54c9e5363f7df605038a
mai2130 <shummai.821@gmail.com>
2025-10-20 06:23:37 +0900

    accountsモデル作成

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.IntegerChoices):
+        NURSERY = 0, '園'
+        GUARDIAN = 1, '保護者'
+    role = models.PositiveSmallIntegerField(choices=Role.choices, default=Role.GUARDIAN)
+    email = models.EmailField(unique=True)
+    
+    def is_nursery(self) -> bool:
+        return self.role == self.Role.GUARDIAN
+    
+    def is_guardian(self) -> bool:
+        return self.role == self.Role.GUARDIAN
+    
+    def __str__(self) -> str:
+        return f'{self.username}({self.get_role_display()})'
