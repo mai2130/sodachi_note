ae94c18e02df54f3ac398544e9e717180e400708
mai2130 <shummai.821@gmail.com>
2025-10-31 17:28:19 +0900

    NURSERYをFACILITYへ修正

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,21 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-class User(AbstractUser):
+class User(AbstractUser): 
+    
     class Role(models.IntegerChoices):
-        NURSERY = 0, '園'
+        FACILITY = 0, '園'
         GUARDIAN = 1, '保護者'
-    role = models.PositiveSmallIntegerField(choices=Role.choices, default=Role.GUARDIAN)
+        
+    role = models.PositiveSmallIntegerField(
+        choices=Role.choices,
+        default=Role.GUARDIAN
+    )
     email = models.EmailField(unique=True)
-    
-    def is_nursery(self) -> bool:
+
+   
+    def is_facility(self):
+        return self.role == self.Role.FACILITY
+
+    def is_guardian(self):
         return self.role == self.Role.GUARDIAN
-    
-    def is_guardian(self) -> bool:
-        return self.role == self.Role.GUARDIAN
-    
-    def __str__(self) -> str:
-        return f'{self.username}({self.get_role_display()})'
