790a09802614818a8b3f1d962777e608d28e17e1
mai2130 <shummai.821@gmail.com>
2025-10-31 17:30:54 +0900

    accountsビュー作成

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import EmailAuthenticationForm
 
-# Create your views here.
+class EmailLoginView(View):
+    template_name = 'registration/login.html'
+
+    def get(self,request):
+        form = EmailAuthenticationForm()
+        return render(request, self.template_name,{'form':form})
+    
+    def post(self, request):
+        form = EmailAuthenticationForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['user']
+            login(request, user)
+            return redirect('home')
+        return render(request, self.template_name,{'form':form})
+    
+        
