bf7b8f977084ff53a397d3586d5ca9cab3697f2f
mai2130 <shummai.821@gmail.com>
2025-11-02 17:12:06 +0900

    logoutURL追記

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-#from django.contrib.auth.views import LogoutView as DjangoLogoutView
-from .views import EmailLoginView
+from django.contrib.auth.views import LogoutView
+from .views import EmailLoginView, FacilitySignUpView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', EmailLoginView.as_view(), name='login'),
-    #path('logout/', DjangoLogoutView.as_view(), name='logout'),
-
+    path('signup/facility/', FacilitySignUpView.as_view(), name='signup_facility'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
