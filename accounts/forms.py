db2c8f2fb65519555ce7e82e19842d98683bfcb9
mai2130 <shummai.821@gmail.com>
2025-11-02 17:14:55 +0900

    FacilitySignUpView追記

--- accounts/forms.py
+++ accounts/forms.py
@@ -28,3 +28,23 @@ class EmailAuthenticationForm(forms.Form):
         cleaned['user'] = user
         return cleaned
     
+class FacilitySignUpForm(forms.Form):
+    nursery_name = forms.CharField(label= "施設名", max_length=100)
+    postal_code = forms.CharField(label ="郵便番号", max_length=8)
+    address = forms.CharField(label = "住所", max_length=100)
+    phone_number = forms.CharField(label = "電話番号", max_length=15)
+    email = forms.EmailField(label = "メールアドレス")
+    password1 = forms.CharField(label = "パスワード ※英数字８文字以上",widget=forms.PasswordInput)
+    password2 = forms.CharField(label = "パスワード（確認用）",widget=forms.PasswordInput)
+    
+        
+    def clean(self):
+        cleaned = super().clean()
+        if cleaned.get("password1")!= cleaned.get("password2"):
+            raise ValidationError("パスワードが一致しません")
+        if User.objects.filter(email=cleaned.get("email")).exists():
+            raise ValidationError("このメールアドレスは既に使われています")
+        return cleaned
+        
+                                                     
+            
