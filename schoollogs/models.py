c7f3ed10d4981ca6f8671970d727b806e18458e0
mai2130 <shummai.821@gmail.com>
2025-11-22 14:39:28 +0900

    園での生活・成長記録モデル作成

--- schoollogs/models.py
+++ schoollogs/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from datetime import date
+#from children.models import Child
 
-# Create your models here.
+class SchoolGrowthLog(models.Model):
+    CONDITION_CHOICES = [
+        (0,"異常なし"),
+        (1,"発熱"),
+        (2,"鼻水"),
+        (3,"咳"),
+        (4,"下痢"),
+        (5,"嘔吐"),
+    ]
+    POO_CHOICES = [
+        (0,"あり"),
+        (1,"なし"),
+    ]
+    
+    LUNCH_CHOICES = [
+        (0,"完食"),
+        (1,"残す"),
+    ]
+    
+    child = models.ForeignKey(
+        "children.Child", 
+        on_delete=models.CASCADE, 
+        related_name='school_growth_logs',
+        verbose_name="園児",
+        )
+    
+    date = models.DateField("日付",default=date.today)
+    condition = models.IntegerField(
+        "体調",
+        choices=CONDITION_CHOICES,
+        default=0,
+        )
+    temperature = models.DecimalField(
+        "体温",
+        max_digits=3,
+        decimal_places=1,
+        null=True, 
+        blank=True,
+        )
+    poo = models.IntegerField(
+        "排便",
+        choices=POO_CHOICES,
+        null=True,
+        blank=True,
+        )   
+    nap_time = models.TimeField(
+        "午睡時間",
+        null=True,
+        blank=True,
+        )
+    lunch = models.IntegerField(
+        "昼食",
+        choices=LUNCH_CHOICES, 
+        null=True,
+        blank=True,
+        ) 
+    state = models.TextField(
+        "様子",
+        max_length=255,
+        blank=True,
+        )
+    created_at = models.DateTimeField("作成日時",auto_now_add=True) 
+    updated_at = models.DateTimeField("更新日時",auto_now=True)
+
+    class Meta:
+        verbose_name = ("園での生活・成長記録")
+        verbose_name_plural = ("園での生活・成長記録")
+        unique_together = ('child', 'date')
+        ordering = ["-date","child"]
+
+    def __str__(self):
+        return f"{self.child.name} {self.log_date}"
