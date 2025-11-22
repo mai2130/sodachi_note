acb95479f188097c10f407110397845cf7828849
mai2130 <shummai.821@gmail.com>
2025-11-22 14:45:34 +0900

    schoollogsurl追記

--- sodachi_note/urls.py
+++ sodachi_note/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include(('accounts.urls',"accounts"), namespace="accounts")),
     path('accounts/', include('django.contrib.auth.urls')),#ログイン/ログアウト/パスワード再設定
-    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+    path("schoollogs/",include("schoollogs.urls")),
+    path('home/', include("dashboard.urls")),
     path('', include("portfolio.urls")), #トップページ
 ]
