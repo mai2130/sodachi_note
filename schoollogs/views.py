d33131a80b64c93a97b6029facd1e7fbd7f731a4
mai2130 <shummai.821@gmail.com>
2025-11-22 14:41:15 +0900

    園での生活・成長記録作成ビュー作成

--- schoollogs/views.py
+++ schoollogs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import SchoolGrowthLogForm
+from .models import SchoolGrowthLog 
+
+class SchoolGrowthLogCreateView(LoginRequiredMixin, CreateView):
+    model = SchoolGrowthLog
+    form_class = SchoolGrowthLogForm
+    template_name = 'schoollogs/school_growthlog_form.html'
+    success_url = reverse_lazy('schholloogs:school_growthlog_create')
+    
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['date'] = date.today()
+        return initial
