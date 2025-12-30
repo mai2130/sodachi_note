4d81d633bd79af34180647e02532cf8319b3c39c
mai2130 <shummai.821@gmail.com>
2025-12-31 08:12:57 +0900

    保護者登録URL追記

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import EmailLoginView, FacilitySignUpView
+from .views import EmailLoginView, FacilitySignUpView, GuardianSignUpView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', EmailLoginView.as_view(), name='login'),
     path('signup/facility/', FacilitySignUpView.as_view(), name='signup_facility'),
+    path('signup/guardian/', GuardianSignUpView.as_view(), name='signup_guardian'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
