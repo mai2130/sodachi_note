d9c4c5189b395ef2178e6793ea6d6ee5ca56376f
mai2130 <shummai.821@gmail.com>
2026-01-02 02:16:22 +0900

    新規作成画面追記

--- invites/urls.py
+++ invites/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
-from .views import AccountListView, AccountManageView
+from django.urls import path # path("URLの形", 動かすView, name="名前")
+from .views import AccountListView, AccountManageView, AccountCreateView
 
 app_name = 'invites'
 
 urlpatterns = [
-    path('accounts/', AccountListView.as_view(), name='account_list'),
-    path('accounts/<int:pk>/', AccountManageView.as_view(), name='account_manage'),
+    path('accounts/', AccountListView.as_view(), name='account_list'), # 一覧画面
+    path('accounts/new/', AccountCreateView.as_view(), name='account_new'), # 新規作成画面
+    path('accounts/<int:pk>/', AccountManageView.as_view(), name='account_manage'), # 編集画面
 ]
