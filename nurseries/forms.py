0873e967490ba762afc400d338492a850baca64e
mai2130 <shummai.821@gmail.com>
2026-01-02 02:27:51 +0900

    widgets追記

--- nurseries/forms.py
+++ nurseries/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Nursery
 
-class NurseryForm(forms.form):
+class NurseryMyPageForm(forms.ModelForm):
     class Meta:
         model =Nursery
-        fields = ["name", "postal_code", "address", "phone_number","email"]
+        fields = ["name", "postal_code", "address", "phone_number"] 
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "mypage-input"}),
+            "postal_code": forms.TextInput(attrs={"class": "mypage-input"}),
+            "address": forms.TextInput(attrs={"class": "mypage-input"}),
+            "phone_number": forms.TextInput(attrs={"class": "mypage-input"}),
+        }
