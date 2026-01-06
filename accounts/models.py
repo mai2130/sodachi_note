4ae199cd9fb384f9e4ed20b42088304dd24bffe0
mai2130 <shummai.821@gmail.com>
2026-01-07 03:11:28 +0900

    微調整

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,17 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from children.models import Child
 
 class User(AbstractUser): 
+    
+    active_child = models.ForeignKey(
+        Child,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="active_users",
+        verbose_name="現在選択中の園児"
+        )
     
     class Role(models.IntegerChoices):
         FACILITY = 0, '園'
@@ -12,7 +22,6 @@ class User(AbstractUser):
         default=Role.GUARDIAN
     )
     email = models.EmailField(unique=True)
-
    
     def is_facility(self):
         return self.role == self.Role.FACILITY
