d836ad70d2d9ce57338e4806bb7399af2c7ea1e4
mai2130 <shummai.821@gmail.com>
2026-01-19 05:52:59 +0900

    家庭での生活・成長記録追記

--- schoollogs/forms.py
+++ schoollogs/forms.py
@@ -1,6 +1,6 @@
 from datetime import time
 from django import forms
-from .models import SchoolGrowthLog
+from .models import GrowthLog
 
 def ten_minute_choices():
     choices = []
@@ -13,30 +13,83 @@ def ten_minute_choices():
 
 class SchoolGrowthLogForm(forms.ModelForm):
     class Meta:
-        model = SchoolGrowthLog
+        model = GrowthLog
         fields = [
             'child',
             'date',
-            'lunch',
-            'nap_start',
-            'nap_end',
-            'poo',
-            'condition',
-            'temperature',
-            'state',
+            'school_lunch',
+            'school_nap_start',
+            'school_nap_end',
+            'school_poo',
+            'school_condition',
+            'school_temperature',
+            'school_state',
         ]
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
-            'nap_start': forms.TimeInput(attrs={'type': 'time', 'step': 600}), #10分単位
-            'nap_end': forms.TimeInput(attrs={'type': 'time', 'step': 600}), #10分単位           
-            'state': forms.Textarea(attrs={'rows': 4}),
-            'poo': forms.RadioSelect(),
-            'lunch': forms.RadioSelect(),
+            'school_nap_start': forms.TimeInput(attrs={'type': 'time', 'step': 600}), #10分単位
+            'school_nap_end': forms.TimeInput(attrs={'type': 'time', 'step': 600}), #10分単位           
+            'school_state': forms.Textarea(attrs={'rows': 4}),
+            'school_poo': forms.RadioSelect(),
+            'school_lunch': forms.RadioSelect(),
         }
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args,**kwargs)
+                
+        self.fields["school_nap_start"].widget = forms.Select(choices=ten_minute_choices())
+        self.fields["school_nap_end"].widget = forms.Select(choices=ten_minute_choices())
+
+class HomeGrowthLogForm(forms.ModelForm):
+    home_bedtime = forms.ChoiceField(
+        choices=ten_minute_choices(),
+        required=False,
+        label="就寝時間"
+    )
+    home_wake_up_time = forms.ChoiceField(
+        choices=ten_minute_choices(),
+        required=False,
+        label="起床時間"
+    )
+
+    class Meta:
+        model = GrowthLog
+        fields = [
+            "home_bedtime",
+            "home_wake_up_time",
+            "home_appetite",
+            "home_condition",
+            "home_temperature",
+            "home_poo",
+            "home_state",
+        ]
+        widgets = {
+            "home_appetite": forms.RadioSelect(),
+            "home_condition": forms.Select,
+            "home_poo": forms.RadioSelect(),
+            "home_state": forms.Textarea(attrs={"rows": 4}),
+        }
         
-        self.fields["nap_start"].widget = forms.Select(choices=ten_minute_choices())
-        self.fields["nap_end"].widget = forms.Select(choices=ten_minute_choices())
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         
+        if self.instance and self.instance.home_bedtime:
+            self.initial["home_bedtime"] = self.instance.home_bedtime.strftime("%H:%M")
+        if self.instance and self.instance.home_wake_up_time:
+            self.initial["home_wake_up_time"] = self.instance.home_wake_up_time.strftime("%H:%M")
+            
+        if self.instance and getattr(self.instance, "submitted", False):
+            for f in self.fields.values():
+                f.disabled = True
+
+    def _clean_time(self, v):
+        if not v:
+            return None
+        h, m = map(int, v.split(":"))
+        return time(h, m)
+
+    def clean_home_bedtime(self):
+        return self._clean_time(self.cleaned_data.get("home_bedtime"))
+
+    def clean_home_wake_up_time(self):
+        return self._clean_time(self.cleaned_data.get("home_wake_up_time"))
