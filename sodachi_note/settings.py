9d6f5b81ab4f657eb0138eb424acd6e01679de37
mai2130 <shummai.821@gmail.com>
2026-01-31 02:45:39 +0900

    MEDIA追記

--- sodachi_note/settings.py
+++ sodachi_note/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -141,3 +144,4 @@ LOGIN_URL =  'login'
 
 #パスワード再発行（開発中はコンソールにメール出力）
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
