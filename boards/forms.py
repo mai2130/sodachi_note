e4c7d60f200f83f0c45f07e0a18525cc080a23ff
mai2130 <shummai.821@gmail.com>
2026-02-02 01:29:03 +0900

    園児用画面追記

--- boards/forms.py
+++ boards/forms.py
@@ -4,7 +4,7 @@ from .models import Board
 class BoardForm(forms.ModelForm):
     class Meta:
         model = Board
-        fields = ["date", "title", "category"]
+        fields = ["title", "category"]
         widgets = {
             "date":forms.DateInput(
                 attrs={"type":"date", "placeholder":"20YY/MM/DD"}
