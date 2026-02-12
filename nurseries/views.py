add2378b8c957213d6142840e9240d45f73a2b33
mai2130 <shummai.821@gmail.com>
2026-02-13 04:31:52 +0900

    dispatch追記

--- nurseries/views.py
+++ nurseries/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import UpdateView #「編集（更新）画面」作成クラス、フォーム表示（GET）と保存（POST）を自動で実装
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.shortcuts import redirect
 
 from .models import Nursery
 from .forms import NurseryMyPageForm
@@ -10,7 +11,7 @@ class NurseryMyPageView(LoginRequiredMixin, UpdateView):
     model = Nursery
     form_class = NurseryMyPageForm
     template_name = "nurseries/mypage.html"
-    success_url = reverse_lazy("nurseries:mypage")
+    success_url = reverse_lazy("nurseries:nursery_mypage")
     
     def get_object(self, queryset =None):
         #ログインユーザーに紐づく園を編集対象にする
@@ -19,3 +20,10 @@ class NurseryMyPageView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         messages.success(self.request, "保存しました！")
         return super().form_valid(form)
+    
+    def dispatch(self, request, *args, **kwargs):
+        # 保護者が来たら弾く
+        if hasattr(request.user, "is_guardian") and request.user.is_guardian():
+            messages.error(request, "このページは園アカウント専用です")
+            return redirect("dashboard:home")
+        return super().dispatch(request, *args, **kwargs)
