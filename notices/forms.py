437a3a246d43936622d160d19b06e2a9ce66f287
mai2130 <shummai.821@gmail.com>
2026-03-04 02:23:02 +0900

    クラス名プルダウン修正

--- notices/forms.py
+++ notices/forms.py
@@ -3,18 +3,28 @@ from .models import Notice
 from nurseries.models import Classroom
 
 class NoticeForm(forms.ModelForm):
-    classrooms = forms.ModelMultipleChoiceField( #クラスを複数選択可能にする
-        queryset=Classroom.objects.all(),
-        widget=forms.SelectMultiple(attrs={"class":"select-input"}),
+    classroom = forms.ModelChoiceField( 
+        queryset=Classroom.objects.none(),
         required=False,
         label="クラス名",
+        empty_label="全園児",
+        widget=forms.Select(attrs={"class": "select-input"}),
     )
+    
+    def __init__(self, *args, nursery=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if nursery:
+            self.fields["classroom"].queryset = Classroom.objects.filter(nursery=nursery)
+
+        if self.instance and self.instance.pk:
+            first = self.instance.classrooms.first()
+            self.fields["classroom"].initial = first
     
     class Meta:
         model = Notice
-        fields = ['date','title','category', 'classrooms', 'body','file']
+        fields = ['date','title','category', 'classroom', 'body','file']
         widgets = {
-            'date' : forms.DateInput(attrs={'type': 'date'}),
+            'date' : forms.DateInput(attrs={'type': 'date', 'class' : 'text-input'}),
             'title' : forms.TextInput(attrs={'class': 'text-input'}),
             'category' : forms.Select(attrs={'class': 'select-input'}),
             'body' : forms.Textarea(attrs={
