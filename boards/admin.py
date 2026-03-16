6f04e1e3081cdf1415b49585694e8acff0ff4124
mai2130 <shummai.821@gmail.com>
2026-03-17 05:47:37 +0900

    管理画面フィルター追加

--- boards/admin.py
+++ boards/admin.py
@@ -5,9 +5,24 @@ from .models import Board, BoardPost
 class BoardAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'category', 'date', 'nursery', 'user', 'created_at')
     list_filter = ('category', 'date', 'nursery')
-    search_fields = ('title',)
+    search_fields = ('title', 'user__username')
+    ordering = ('-date', '-created_at')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(nursery=request.user.nursery)
 
 @admin.register(BoardPost)
 class BoardPostAdmin(admin.ModelAdmin):
     list_display = ('id', 'board', 'user', 'comment', 'created_at')
-    search_fields = ('board' ,'user')
+    list_filter = ('created_at',)
+    search_fields = ('board__title' ,'user__username', 'comment')
+    ordering = ('-created_at',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(nursery=request.user.nursery)
