195bcf55c0b9ef63efa9967a055c3f9ac3f61df8
mai2130 <shummai.821@gmail.com>
2026-04-29 17:08:55 +0900

    クラス名追記

--- nurseries/admin.py
+++ nurseries/admin.py
@@ -3,10 +3,10 @@ from .models import Nursery, Classroom
 
 @admin.register(Nursery)
 class NurseryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name','user')
     
 @admin.register(Classroom)
 class ClassroomAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'nursery','created_at')   
     list_filter = ('nursery',)  
-    search_fields = ('name',) 
+    search_fields = ('name','nursery__name') 
